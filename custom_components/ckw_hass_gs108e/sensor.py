99a669a061fe53d35e70c9ea8dc6f146b538d81d
Christian Karrié <christian@karrie.info>
2023-01-05 21:55:34 +0100

    morrrre sensors

--- custom_components/ckw_hass_gs108e/sensor.py
+++ custom_components/ckw_hass_gs108e/sensor.py
@@ -72,6 +72,18 @@ DEVICE_SENSOR_TYPES = [
         device_class=SensorDeviceClass.DURATION,
         icon="mdi:clock",
     ),
+    
+    #NetgearSensorEntityDescription(
+    #    key="NewOOKLADownlinkBandwidth",
+    #    name="Downlink Bandwidth",
+    #    entity_category=EntityCategory.DIAGNOSTIC,
+    #    native_unit_of_measurement=UnitOfDataRate.MEGABITS_PER_SECOND,
+    #    device_class=SensorDeviceClass.DATA_RATE,
+    #    icon="mdi:download",
+    #),
+]
+
+PORT_SENSORS = [
     NetgearSensorEntityDescription(
         key="port_1_traffic_rx_bytes",
         name="Port 1 Traffic Received",
@@ -88,14 +100,310 @@ DEVICE_SENSOR_TYPES = [
         device_class=SensorDeviceClass.DATA_SIZE,
         icon="mdi:upload",
     ),
-    #NetgearSensorEntityDescription(
-    #    key="NewOOKLADownlinkBandwidth",
-    #    name="Downlink Bandwidth",
-    #    entity_category=EntityCategory.DIAGNOSTIC,
-    #    native_unit_of_measurement=UnitOfDataRate.MEGABITS_PER_SECOND,
-    #    device_class=SensorDeviceClass.DATA_RATE,
-    #    icon="mdi:download",
-    #),
+    NetgearSensorEntityDescription(
+        key="port_1_speed_rx_bytes",
+        name="Port 1 Receiving",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        native_unit_of_measurement=UnitOfDataRate.BYTES_PER_SECOND,
+        device_class=SensorDeviceClass.DATA_RATE,
+        icon="mdi:download",
+    ),
+    NetgearSensorEntityDescription(
+        key="port_1_speed_tx_bytes",
+        name="Port 1 Transferring",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        native_unit_of_measurement=UnitOfDataRate.BYTES_PER_SECOND,
+        device_class=SensorDeviceClass.DATA_RATE,
+        icon="mdi:upload",
+    ),
+    NetgearSensorEntityDescription(
+        key="port_1_speed_io_bytes",
+        name="Port 1 IO",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        native_unit_of_measurement=UnitOfDataRate.BYTES_PER_SECOND,
+        device_class=SensorDeviceClass.DATA_RATE,
+        icon="mdi:swap-vertical",
+    ),
+    NetgearSensorEntityDescription(
+        key="port_2_traffic_rx_bytes",
+        name="Port 2 Traffic Received",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        native_unit_of_measurement=UnitOfInformation.BYTES,
+        device_class=SensorDeviceClass.DATA_SIZE,
+        icon="mdi:download",
+    ),
+    NetgearSensorEntityDescription(
+        key="port_2_traffic_tx_bytes",
+        name="Port 2 Traffic Transferred",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        native_unit_of_measurement=UnitOfInformation.BYTES,
+        device_class=SensorDeviceClass.DATA_SIZE,
+        icon="mdi:upload",
+    ),
+    NetgearSensorEntityDescription(
+        key="port_2_speed_rx_bytes",
+        name="Port 2 Receiving",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        native_unit_of_measurement=UnitOfDataRate.BYTES_PER_SECOND,
+        device_class=SensorDeviceClass.DATA_RATE,
+        icon="mdi:download",
+    ),
+    NetgearSensorEntityDescription(
+        key="port_2_speed_tx_bytes",
+        name="Port 2 Transferring",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        native_unit_of_measurement=UnitOfDataRate.BYTES_PER_SECOND,
+        device_class=SensorDeviceClass.DATA_RATE,
+        icon="mdi:upload",
+    ),
+    NetgearSensorEntityDescription(
+        key="port_2_speed_io_bytes",
+        name="Port 2 IO",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        native_unit_of_measurement=UnitOfDataRate.BYTES_PER_SECOND,
+        device_class=SensorDeviceClass.DATA_RATE,
+        icon="mdi:swap-vertical",
+    ),
+    NetgearSensorEntityDescription(
+        key="port_3_traffic_rx_bytes",
+        name="Port 3 Traffic Received",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        native_unit_of_measurement=UnitOfInformation.BYTES,
+        device_class=SensorDeviceClass.DATA_SIZE,
+        icon="mdi:download",
+    ),
+    NetgearSensorEntityDescription(
+        key="port_3_traffic_tx_bytes",
+        name="Port 3 Traffic Transferred",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        native_unit_of_measurement=UnitOfInformation.BYTES,
+        device_class=SensorDeviceClass.DATA_SIZE,
+        icon="mdi:upload",
+    ),
+    NetgearSensorEntityDescription(
+        key="port_3_speed_rx_bytes",
+        name="Port 3 Receiving",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        native_unit_of_measurement=UnitOfDataRate.BYTES_PER_SECOND,
+        device_class=SensorDeviceClass.DATA_RATE,
+        icon="mdi:download",
+    ),
+    NetgearSensorEntityDescription(
+        key="port_3_speed_tx_bytes",
+        name="Port 3 Transferring",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        native_unit_of_measurement=UnitOfDataRate.BYTES_PER_SECOND,
+        device_class=SensorDeviceClass.DATA_RATE,
+        icon="mdi:upload",
+    ),
+    NetgearSensorEntityDescription(
+        key="port_3_speed_io_bytes",
+        name="Port 3 IO",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        native_unit_of_measurement=UnitOfDataRate.BYTES_PER_SECOND,
+        device_class=SensorDeviceClass.DATA_RATE,
+        icon="mdi:swap-vertical",
+    ),
+    NetgearSensorEntityDescription(
+        key="port_4_traffic_rx_bytes",
+        name="Port 4 Traffic Received",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        native_unit_of_measurement=UnitOfInformation.BYTES,
+        device_class=SensorDeviceClass.DATA_SIZE,
+        icon="mdi:download",
+    ),
+    NetgearSensorEntityDescription(
+        key="port_4_traffic_tx_bytes",
+        name="Port 4 Traffic Transferred",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        native_unit_of_measurement=UnitOfInformation.BYTES,
+        device_class=SensorDeviceClass.DATA_SIZE,
+        icon="mdi:upload",
+    ),
+    NetgearSensorEntityDescription(
+        key="port_4_speed_rx_bytes",
+        name="Port 4 Receiving",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        native_unit_of_measurement=UnitOfDataRate.BYTES_PER_SECOND,
+        device_class=SensorDeviceClass.DATA_RATE,
+        icon="mdi:download",
+    ),
+    NetgearSensorEntityDescription(
+        key="port_4_speed_tx_bytes",
+        name="Port 4 Transferring",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        native_unit_of_measurement=UnitOfDataRate.BYTES_PER_SECOND,
+        device_class=SensorDeviceClass.DATA_RATE,
+        icon="mdi:upload",
+    ),
+    NetgearSensorEntityDescription(
+        key="port_4_speed_io_bytes",
+        name="Port 4 IO",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        native_unit_of_measurement=UnitOfDataRate.BYTES_PER_SECOND,
+        device_class=SensorDeviceClass.DATA_RATE,
+        icon="mdi:swap-vertical",
+    ),
+    NetgearSensorEntityDescription(
+        key="port_5_traffic_rx_bytes",
+        name="Port 5 Traffic Received",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        native_unit_of_measurement=UnitOfInformation.BYTES,
+        device_class=SensorDeviceClass.DATA_SIZE,
+        icon="mdi:download",
+    ),
+    NetgearSensorEntityDescription(
+        key="port_5_traffic_tx_bytes",
+        name="Port 5 Traffic Transferred",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        native_unit_of_measurement=UnitOfInformation.BYTES,
+        device_class=SensorDeviceClass.DATA_SIZE,
+        icon="mdi:upload",
+    ),
+    NetgearSensorEntityDescription(
+        key="port_5_speed_rx_bytes",
+        name="Port 5 Receiving",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        native_unit_of_measurement=UnitOfDataRate.BYTES_PER_SECOND,
+        device_class=SensorDeviceClass.DATA_RATE,
+        icon="mdi:download",
+    ),
+    NetgearSensorEntityDescription(
+        key="port_5_speed_tx_bytes",
+        name="Port 5 Transferring",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        native_unit_of_measurement=UnitOfDataRate.BYTES_PER_SECOND,
+        device_class=SensorDeviceClass.DATA_RATE,
+        icon="mdi:upload",
+    ),
+    NetgearSensorEntityDescription(
+        key="port_5_speed_io_bytes",
+        name="Port 5 IO",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        native_unit_of_measurement=UnitOfDataRate.BYTES_PER_SECOND,
+        device_class=SensorDeviceClass.DATA_RATE,
+        icon="mdi:swap-vertical",
+    ),
+    NetgearSensorEntityDescription(
+        key="port_6_traffic_rx_bytes",
+        name="Port 6 Traffic Received",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        native_unit_of_measurement=UnitOfInformation.BYTES,
+        device_class=SensorDeviceClass.DATA_SIZE,
+        icon="mdi:download",
+    ),
+    NetgearSensorEntityDescription(
+        key="port_6_traffic_tx_bytes",
+        name="Port 6 Traffic Transferred",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        native_unit_of_measurement=UnitOfInformation.BYTES,
+        device_class=SensorDeviceClass.DATA_SIZE,
+        icon="mdi:upload",
+    ),
+    NetgearSensorEntityDescription(
+        key="port_6_speed_rx_bytes",
+        name="Port 6 Receiving",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        native_unit_of_measurement=UnitOfDataRate.BYTES_PER_SECOND,
+        device_class=SensorDeviceClass.DATA_RATE,
+        icon="mdi:download",
+    ),
+    NetgearSensorEntityDescription(
+        key="port_6_speed_tx_bytes",
+        name="Port 6 Transferring",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        native_unit_of_measurement=UnitOfDataRate.BYTES_PER_SECOND,
+        device_class=SensorDeviceClass.DATA_RATE,
+        icon="mdi:upload",
+    ),
+    NetgearSensorEntityDescription(
+        key="port_6_speed_io_bytes",
+        name="Port 6 IO",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        native_unit_of_measurement=UnitOfDataRate.BYTES_PER_SECOND,
+        device_class=SensorDeviceClass.DATA_RATE,
+        icon="mdi:swap-vertical",
+    ),
+    NetgearSensorEntityDescription(
+        key="port_7_traffic_rx_bytes",
+        name="Port 7 Traffic Received",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        native_unit_of_measurement=UnitOfInformation.BYTES,
+        device_class=SensorDeviceClass.DATA_SIZE,
+        icon="mdi:download",
+    ),
+    NetgearSensorEntityDescription(
+        key="port_7_traffic_tx_bytes",
+        name="Port 7 Traffic Transferred",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        native_unit_of_measurement=UnitOfInformation.BYTES,
+        device_class=SensorDeviceClass.DATA_SIZE,
+        icon="mdi:upload",
+    ),
+    NetgearSensorEntityDescription(
+        key="port_7_speed_rx_bytes",
+        name="Port 7 Receiving",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        native_unit_of_measurement=UnitOfDataRate.BYTES_PER_SECOND,
+        device_class=SensorDeviceClass.DATA_RATE,
+        icon="mdi:download",
+    ),
+    NetgearSensorEntityDescription(
+        key="port_7_speed_tx_bytes",
+        name="Port 7 Transferring",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        native_unit_of_measurement=UnitOfDataRate.BYTES_PER_SECOND,
+        device_class=SensorDeviceClass.DATA_RATE,
+        icon="mdi:upload",
+    ),
+    NetgearSensorEntityDescription(
+        key="port_7_speed_io_bytes",
+        name="Port 7 IO",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        native_unit_of_measurement=UnitOfDataRate.BYTES_PER_SECOND,
+        device_class=SensorDeviceClass.DATA_RATE,
+        icon="mdi:swap-vertical",
+    ),
+    NetgearSensorEntityDescription(
+        key="port_8_traffic_rx_bytes",
+        name="Port 8 Traffic Received",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        native_unit_of_measurement=UnitOfInformation.BYTES,
+        device_class=SensorDeviceClass.DATA_SIZE,
+        icon="mdi:download",
+    ),
+    NetgearSensorEntityDescription(
+        key="port_8_traffic_tx_bytes",
+        name="Port 8 Traffic Transferred",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        native_unit_of_measurement=UnitOfInformation.BYTES,
+        device_class=SensorDeviceClass.DATA_SIZE,
+        icon="mdi:upload",
+    ),
+    NetgearSensorEntityDescription(
+        key="port_8_speed_rx_bytes",
+        name="Port 8 Receiving",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        native_unit_of_measurement=UnitOfDataRate.BYTES_PER_SECOND,
+        device_class=SensorDeviceClass.DATA_RATE,
+        icon="mdi:download",
+    ),
+    NetgearSensorEntityDescription(
+        key="port_8_speed_tx_bytes",
+        name="Port 8 Transferring",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        native_unit_of_measurement=UnitOfDataRate.BYTES_PER_SECOND,
+        device_class=SensorDeviceClass.DATA_RATE,
+        icon="mdi:upload",
+    ),
+    NetgearSensorEntityDescription(
+        key="port_8_speed_io_bytes",
+        name="Port 8 IO",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        native_unit_of_measurement=UnitOfDataRate.BYTES_PER_SECOND,
+        device_class=SensorDeviceClass.DATA_RATE,
+        icon="mdi:swap-vertical",
+    ),
 ]
 
 
@@ -114,6 +422,11 @@ async def async_setup_entry(
             NetgearRouterSensorEntity(coordinator_switch_infos, gs_switch, description)
         )
 
+    for description in PORT_SENSORS:
+        switch_entities.append(
+            NetgearRouterSensorEntity(coordinator_switch_infos, gs_switch, description)
+        )
+
     async_add_entities(switch_entities)
 
     # Entities per network device
@@ -126,7 +439,7 @@ async def async_setup_entry(
 class NetgearRouterSensorEntity(HAGS108SwitchCoordinatorEntity, RestoreSensor):
     """Representation of a device connected to a Netgear router."""
 
-    _attr_entity_registry_enabled_default = False
+    #_attr_entity_registry_enabled_default = False
     entity_description: NetgearSensorEntityDescription
 
     def __init__(
